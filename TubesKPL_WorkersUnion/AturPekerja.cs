8e7d475ba34b6a82cae0e25bac19ce0cba5c1eaa
SirTonyEdgar <daffamuhammad625@gmail.com>
2024-05-15 23:38:21 +0700

    coba submit AturPekerja

---
+++ TubesKPL_WorkersUnion/AturPekerja.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TubesKPL_WorkersUnion
+{
+    public class AturPekerja
+    {
+        private List<Pengguna> daftarPengguna;
+
+        public AturPekerja()
+        {
+            daftarPengguna = new List<Pengguna>();
+        }
+
+        public void TambahPengguna(Pengguna pengguna)
+        {
+            daftarPengguna.Add(pengguna);
+        }
+
+        public Pengguna CariPengguna(string username)
+        {
+            return daftarPengguna.FirstOrDefault(p => p.username == username);
+        }
+
+        public void HapusPengguna(string username)
+        {
+            Pengguna pengguna = CariPengguna(username);
+            if (pengguna != null)
+            {
+                daftarPengguna.Remove(pengguna);
+            }
+        }
+
+        public void TampilkanSemuaPengguna()
+        {
+            foreach (var pengguna in daftarPengguna)
+            {
+                Console.WriteLine($"Username: {pengguna.username}, Full Name: {pengguna.fullname}");
+            }
+        }
+    }
+}
