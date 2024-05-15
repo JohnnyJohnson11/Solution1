3a8b7f161adffde2f11bb9c4092dbb2ad08dba48
Farid Hanafi <cruztea@student.telkomuniversity.ac.id>
2024-05-15 21:31:27 +0700

    Class Menu

---
+++ TubesKPL_WorkersUnion/Class1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TubesKPL_Kelompok8;
+
+namespace menu
+{
+    class Class1 {
+        public void menu()
+        {
+            Console.WriteLine("1. Login");
+            Console.WriteLine("2. Register");
+            Console.WriteLine("0. Keluar");
+            string input = Console.ReadLine();
+            while (input != "0")
+            {
+                if (input == "1")
+                {
+                    bool found = false;
+                    while (!found)
+                    {
+                        Console.WriteLine("Masukkan Username: ");
+                        string username = Console.ReadLine();
+                        Console.WriteLine("Masukkan Password: ");
+                        string password = Console.ReadLine();
+                        LoginConfig loginConfig = new LoginConfig();
+                        loginConfig.ReadConfigFile();
+                        int i;
+                        for (i = 0; i < loginConfig.ListPengguna.pengguna.Count; i++)
+                        {
+                            if (loginConfig.ListPengguna.pengguna[i].username == username && password == loginConfig.ListPengguna.pengguna[i].password)
+                            {
+                                Console.WriteLine("Email dan Password benar");
+                                found = true;
+                                break;
+                            }
+                        }
+                        if (!found)
+                        {
+                            Console.WriteLine("Email atau Password salah");
+                        }
+                    }
+
+
+                }
+                else if (input == "2")
+                {
+                    Console.WriteLine("Full name: ");
+                    string fullname = Console.ReadLine();
+                    Console.WriteLine("Username: ");
+                    string username = Console.ReadLine();
+                    Console.WriteLine("Email: ");
+                    string email = Console.ReadLine();
+                    Console.WriteLine("Password: ");
+                    string password = Console.ReadLine();
+                    bool valid = false;
+                    while (!valid)
+                    {
+                        try
+                        {
+                            if (int.TryParse(password, out _))
+                            {
+                                throw new FormatException("Password harus ada huruf");
+                            }
+                            valid = true;
+                        }
+                        catch (FormatException ex)
+                        {
+                            Console.WriteLine(ex.Message);
+                            Console.WriteLine("Password: ");
+                            password = Console.ReadLine();
+                        }
+                    }
+                    UnggahPekerjaanConfig config = new UnggahPekerjaanConfig();
+                    config.Register(fullname, username, email, password);
+                    Console.WriteLine("Akun Terdaftar");
+                }
+                Console.WriteLine("1. Login");
+                Console.WriteLine("2. Register");
+                Console.WriteLine("0. Keluar");
+                input = Console.ReadLine();
+            }
+        }
+    }
+}
