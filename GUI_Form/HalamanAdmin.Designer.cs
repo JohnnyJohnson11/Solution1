730e806ecfa387401e364175dc32cbf78a8a7450
Farid Hanafi <cruztea@student.telkomuniversity.ac.id>
2024-06-18 20:47:59 +0700

    menambahkan gui lagi (lagi) (lagi)

---
+++ GUI_Form/HalamanAdmin.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace GUI_Form
+{
+    partial class HalamanAdmin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(HalamanAdmin));
+            pictureBox1 = new PictureBox();
+            richTextBox2 = new RichTextBox();
+            listBox1 = new ListBox();
+            label2 = new Label();
+            label4 = new Label();
+            button1 = new Button();
+            button3 = new Button();
+            button5 = new Button();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(-9, -2);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(978, 561);
+            pictureBox1.TabIndex = 82;
+            pictureBox1.TabStop = false;
+            // 
+            // richTextBox2
+            // 
+            richTextBox2.Font = new Font("Times New Roman", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            richTextBox2.Location = new Point(284, 118);
+            richTextBox2.Name = "richTextBox2";
+            richTextBox2.Size = new Size(420, 378);
+            richTextBox2.TabIndex = 84;
+            richTextBox2.Text = "";
+            // 
+            // listBox1
+            // 
+            listBox1.Font = new Font("Times New Roman", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            listBox1.FormattingEnabled = true;
+            listBox1.ItemHeight = 22;
+            listBox1.Location = new Point(66, 118);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(212, 378);
+            listBox1.TabIndex = 83;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.BackColor = SystemColors.GrayText;
+            label2.Font = new Font("Times New Roman", 16.2F, FontStyle.Bold, GraphicsUnit.Point);
+            label2.ForeColor = Color.Transparent;
+            label2.Location = new Point(65, 22);
+            label2.Name = "label2";
+            label2.Size = new Size(278, 32);
+            label2.TabIndex = 85;
+            label2.Text = "Verifikasi Perusahaan";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.BackColor = Color.Transparent;
+            label4.Font = new Font("Times New Roman", 13.8F, FontStyle.Bold, GraphicsUnit.Point);
+            label4.ForeColor = Color.Black;
+            label4.Location = new Point(65, 64);
+            label4.Name = "label4";
+            label4.Size = new Size(596, 25);
+            label4.TabIndex = 97;
+            label4.Text = "Berikut adalah list perusahaan yang memerlukan tindakan";
+            label4.Click += this.label4_Click;
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.Red;
+            button1.Font = new Font("Times New Roman", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            button1.ForeColor = Color.White;
+            button1.Location = new Point(750, 160);
+            button1.Name = "button1";
+            button1.Size = new Size(198, 36);
+            button1.TabIndex = 99;
+            button1.Text = "Tolak";
+            button1.UseVisualStyleBackColor = false;
+            // 
+            // button3
+            // 
+            button3.BackColor = Color.LimeGreen;
+            button3.Font = new Font("Times New Roman", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            button3.Location = new Point(750, 118);
+            button3.Name = "button3";
+            button3.Size = new Size(198, 36);
+            button3.TabIndex = 98;
+            button3.Text = "Terima";
+            button3.UseVisualStyleBackColor = false;
+            // 
+            // button5
+            // 
+            button5.BackColor = Color.Red;
+            button5.Font = new Font("Times New Roman", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            button5.ForeColor = Color.White;
+            button5.Location = new Point(12, 506);
+            button5.Name = "button5";
+            button5.Size = new Size(121, 39);
+            button5.TabIndex = 100;
+            button5.Text = "Log Out";
+            button5.UseVisualStyleBackColor = false;
+            // 
+            // HalamanAdmin
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(960, 557);
+            Controls.Add(button5);
+            Controls.Add(button1);
+            Controls.Add(button3);
+            Controls.Add(label4);
+            Controls.Add(label2);
+            Controls.Add(richTextBox2);
+            Controls.Add(listBox1);
+            Controls.Add(pictureBox1);
+            Name = "HalamanAdmin";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private PictureBox pictureBox1;
+        private RichTextBox richTextBox2;
+        private ListBox listBox1;
+        private Label label2;
+        private Label label4;
+        private Button button1;
+        private Button button3;
+        private Button button5;
+    }
+}
---
+++ GUI_Form/VerifikasiPerusahaan.Designer.cs
@@ -0,0 +1,221 @@
+﻿namespace GUI_Form
+{
+    partial class VerifikasiPerusahaan
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(VerifikasiPerusahaan));
+            button7 = new Button();
+            label2 = new Label();
+            pictureBox1 = new PictureBox();
+            label5 = new Label();
+            label3 = new Label();
+            label1 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            button1 = new Button();
+            label4 = new Label();
+            label6 = new Label();
+            richTextBox1 = new RichTextBox();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // button7
+            // 
+            button7.BackColor = Color.Black;
+            button7.Font = new Font("Times New Roman", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            button7.ForeColor = SystemColors.ButtonFace;
+            button7.Location = new Point(12, 12);
+            button7.Name = "button7";
+            button7.Size = new Size(37, 29);
+            button7.TabIndex = 83;
+            button7.Text = "<";
+            button7.UseVisualStyleBackColor = false;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.BackColor = SystemColors.GrayText;
+            label2.Font = new Font("Times New Roman", 16.2F, FontStyle.Bold, GraphicsUnit.Point);
+            label2.ForeColor = Color.Transparent;
+            label2.Location = new Point(65, 22);
+            label2.Name = "label2";
+            label2.Size = new Size(223, 32);
+            label2.TabIndex = 82;
+            label2.Text = "Isi data Verifikasi";
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(-5, -3);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(978, 561);
+            pictureBox1.TabIndex = 81;
+            pictureBox1.TabStop = false;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Times New Roman", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            label5.Location = new Point(30, 280);
+            label5.Name = "label5";
+            label5.Size = new Size(159, 23);
+            label5.TabIndex = 88;
+            label5.Text = "Aset Perusahaan:";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Times New Roman", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            label3.Location = new Point(30, 214);
+            label3.Name = "label3";
+            label3.Size = new Size(168, 23);
+            label3.TabIndex = 87;
+            label3.Text = "Tanggal Pendirian:";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Times New Roman", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.Location = new Point(30, 147);
+            label1.Name = "label1";
+            label1.Size = new Size(194, 23);
+            label1.TabIndex = 86;
+            label1.Text = "Kategori Perusahaan:";
+            // 
+            // textBox1
+            // 
+            textBox1.Font = new Font("Times New Roman", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            textBox1.Location = new Point(292, 147);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(268, 30);
+            textBox1.TabIndex = 92;
+            textBox1.TextChanged += textBox1_TextChanged;
+            // 
+            // textBox2
+            // 
+            textBox2.Font = new Font("Times New Roman", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            textBox2.Location = new Point(292, 214);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(268, 30);
+            textBox2.TabIndex = 93;
+            // 
+            // textBox3
+            // 
+            textBox3.Font = new Font("Times New Roman", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            textBox3.Location = new Point(292, 280);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(268, 30);
+            textBox3.TabIndex = 94;
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.DodgerBlue;
+            button1.Font = new Font("Times New Roman", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            button1.ForeColor = Color.White;
+            button1.Location = new Point(788, 506);
+            button1.Name = "button1";
+            button1.Size = new Size(160, 39);
+            button1.TabIndex = 95;
+            button1.Text = "Minta Verifikasi";
+            button1.UseVisualStyleBackColor = false;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.BackColor = Color.White;
+            label4.Font = new Font("Times New Roman", 13.8F, FontStyle.Bold, GraphicsUnit.Point);
+            label4.ForeColor = Color.Black;
+            label4.Location = new Point(31, 74);
+            label4.Name = "label4";
+            label4.Size = new Size(303, 25);
+            label4.TabIndex = 96;
+            label4.Text = "Masukkan data di bawah ini:";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Times New Roman", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            label6.Location = new Point(30, 340);
+            label6.Name = "label6";
+            label6.Size = new Size(78, 23);
+            label6.TabIndex = 97;
+            label6.Text = "Alamat:";
+            // 
+            // richTextBox1
+            // 
+            richTextBox1.Font = new Font("Times New Roman", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            richTextBox1.Location = new Point(292, 340);
+            richTextBox1.Name = "richTextBox1";
+            richTextBox1.ReadOnly = true;
+            richTextBox1.Size = new Size(450, 141);
+            richTextBox1.TabIndex = 98;
+            richTextBox1.Text = "";
+            // 
+            // VerifikasiPerusahaan
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(960, 557);
+            Controls.Add(richTextBox1);
+            Controls.Add(label6);
+            Controls.Add(label4);
+            Controls.Add(button1);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(label5);
+            Controls.Add(label3);
+            Controls.Add(label1);
+            Controls.Add(button7);
+            Controls.Add(label2);
+            Controls.Add(pictureBox1);
+            Name = "VerifikasiPerusahaan";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button button7;
+        private Label label2;
+        private PictureBox pictureBox1;
+        private Label label5;
+        private Label label3;
+        private Label label1;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private Button button1;
+        private Label label4;
+        private Label label6;
+        private RichTextBox richTextBox1;
+    }
+}
